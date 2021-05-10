e038845ea0fa13fce79b8a202ec98a4aa10f5ba4
Julian Pustkuchen <Julian@Pustkuchen.com>
2021-05-10 06:35:28 +0200

    Cleanup

--- src/TableFactory.php
+++ src/TableFactory.php
@@ -3,12 +3,11 @@
 namespace JPustkuchen\TableClassBundle;
 
 use JPustkuchen\TableClassBundle\Elements\Table;
+use JPustkuchen\TableClassBundle\Elements\TableRow;
 
-class TableFactory
-{
+class TableFactory {
 
-    public static function createTable(?TableRow $header = null, array $rows = [], array $classes = [], array $attributes = []): Table
-    {
+    public static function createTable(?TableRow $header = null, array $rows = [], array $classes = [], array $attributes = []): Table {
         return new Table($header, $rows, $classes, $attributes);
     }
 }
