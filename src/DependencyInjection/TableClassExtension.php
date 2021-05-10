03f2cbc7afbb91f7d570d952ed1c203440c9a5c4
Julian Pustkuchen <Julian@Pustkuchen.com>
2021-05-10 07:32:13 +0200

    Cleanup

--- src/DependencyInjection/TableClassExtension.php
+++ src/DependencyInjection/TableClassExtension.php
@@ -11,10 +11,7 @@ class TableClassExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-      $loader = new XmlFileLoader(
-        $container,
-        new FileLocator(__DIR__.'/../Resources/config')
-      );
+      $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
       $loader->load('services.xml');
     }
 }
