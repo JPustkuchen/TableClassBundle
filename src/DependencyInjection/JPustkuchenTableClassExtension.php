db1175fc2364fc281a7eddcdbd0b9040bddb35f6
Julian Pustkuchen <Julian@Pustkuchen.com>
2021-05-10 07:23:46 +0200

    Cleanup

---
+++ src/DependencyInjection/JPustkuchenTableClassExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Acme\HelloBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class JPustkuchenTableClassExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+      $loader = new XmlFileLoader(
+        $container,
+        new FileLocator(__DIR__.'/../Resources/config')
+      );
+      $loader->load('services.xml');
+    }
+}
