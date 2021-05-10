60e2a24abc25658182a5dc6f5e884f9cbe43e3a2
Julian Pustkuchen <Julian@Pustkuchen.com>
2021-05-10 10:31:46 +0200

    Cleanup

---
+++ src/DependencyInjection/JPustkuchenTableClassBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace JPustkuchen\TableClassBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class JPustkuchenTableClassExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+      $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+      $loader->load('services.xml');
+      // $containerBuilder->register('tableclass.factory', JPustkuchen\TableClassBundle\TableFactory::class)
+      // ->addArgument('%twig%');
+      // exit('!!!');
+    }
+}
